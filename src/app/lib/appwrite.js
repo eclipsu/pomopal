b79bf2527c9514fc3c13b94a773c79445c90c1d3
eclipsu <stharj219@gmail.com>
2025-11-01 16:36:42 -0500

    CORS fix - changed the endpoint

--- src/app/lib/appwrite.js
+++ src/app/lib/appwrite.js
@@ -1,7 +1,7 @@
 import { Client, Account, Databases, ID } from "appwrite";
 
 const client = new Client()
-  .setEndpoint("https://cloud.appwrite.io/v1") // or your self-hosted URL
+  .setEndpoint("https://fra.cloud.appwrite.io/v1") 
   .setProject("68f95f3f000ba74e0c35");
 
 export const account = new Account(client);
